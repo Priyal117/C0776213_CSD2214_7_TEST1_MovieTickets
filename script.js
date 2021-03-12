468322db398e4e6c0f36b3536acf31371b810a94
Priyal117 <75179145+Priyal117@users.noreply.github.com>
2021-03-11 21:42:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 const container = document.querySelector(".container");
-const count= document.getElementById('no_seats')
-const total = document.getElementById('total_price')
-const selectmovie = document.getElementById('movies')
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const selectmovie = document.getElementById("movie");
+const ticketPrice = document.getElementById("ticket_price");
+let priceofticket = parseInt(selectmovie.value);
 
-let ticketPrice = parseInt(selectmovie.value);
-
-function updatetheseats(){
-    const selectedseats = document.querySelectorAll(".row .selected");
-    const countselectedseats = selectedseats.length;
-    count.innerHTML = countselectedseats;
-    total.innerHTML = countselectedseats * ticketPrice;
+//To count the seats
+function updatetheseats() {
+  const selectedSeats = document.querySelectorAll(".row .selected");
+  const countselectedseats = selectedSeats.length;
+  count.innerHTML = countselectedseats;
+  ticketPrice.innerHTML = priceofticket;
+  //it will multiple the count * priceofticket
+  total.innerHTML = countselectedseats * priceofticket;
 }
 
+
+//it will fetch the value or price of ticket and get call by function
 selectmovie.addEventListener("change", (e) => {
-    ticketPrice = parseInt(e.target.value);
+  priceofticket = parseInt(e.target.value);
+  updatetheseats();
+});
+
+//to fetch the price of movie
+
+
+container.addEventListener("click", (e) => {
+  console.log(e);
+  //to apply event only on class name seat and not on class = occupied
+  if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
+    e.target.classList.toggle("selected");
     updatetheseats();
-
+  }
 });
 
-container.addEventListener("click", function(e){
-    
-        e.target.classList.toggle("selected");
-        updatetheseats();
-    }
-});
+function proceed() {
+      if (!e.target.classList.contains("selected"))  {
+        alert("Nothing is selected");       
+      }
+  }
